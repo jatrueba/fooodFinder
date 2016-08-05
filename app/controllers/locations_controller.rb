47e33093fd9422dbf4c283178354d03156ce5980
Jazmine Trueba <jazmine.mtrueba@gmail.com>
2016-08-05 13:25:23 -0400

    added httparty

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,19 +1,23 @@
+require 'httparty'
 class LocationsController < ApplicationController
+
   def index
 
     @location = Location.all
     @location = request.location
-    @location = Geocoder.search('31405')
+    @location = Geocoder.search('wynwood')
 
     # https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670,151.1957&radius=500&types=food&name=cruise&key=AIzaSyAuw4no8ETufFiyu3FrBgOf4_rtZODlIoU
   end
 
   def create
-    @location = Location.new
   end
 
   def show
-    
+    @location = request.location
+    @location = Geocoder.search('wynwood')
+      @location =  HTTParty.get("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670,151.1957&radius=500&types=food&name=cruise&key=AIzaSyAuw4no8ETufFiyu3FrBgOf4_rtZODlIoU")
   end
+
 
 end
