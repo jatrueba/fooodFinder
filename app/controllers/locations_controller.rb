061c8639c7553ce74049de88ce08b72e2ab36020
Jazmine Trueba <jazmine.mtrueba@gmail.com>
2016-08-05 18:00:29 -0400

    got api to work httparty

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,22 +1,26 @@
 require 'httparty'
+require 'json'
 class LocationsController < ApplicationController
 
   def index
-
-    @location = Location.all
-    @location = request.location
-    @location = Geocoder.search('wynwood')
-
-    # https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670,151.1957&radius=500&types=food&name=cruise&key=AIzaSyAuw4no8ETufFiyu3FrBgOf4_rtZODlIoU
+ # https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670,151.1957&radius=500&types=food&name=cruise&key=AIzaSyAuw4no8ETufFiyu3FrBgOf4_rtZODlIoU
   end
 
   def create
   end
 
   def show
-    @location = request.location
-    @location = Geocoder.search('wynwood')
-      @location =  HTTParty.get("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670,151.1957&radius=500&types=food&name=cruise&key=AIzaSyAuw4no8ETufFiyu3FrBgOf4_rtZODlIoU")
+    # @location = request.location
+    @location = Geocoder.search(params[:search_locations])
+    lat = @location[0].latitude
+    lat_s = lat.to_s
+    long = @location[0].longitude
+    p lat_s.class
+    long_s = long.to_s
+      response =  HTTParty.get("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=#{lat_s},#{long_s}&type=restaurant&radius=1000&key=AIzaSyAuw4no8ETufFiyu3FrBgOf4_rtZODlIoU")
+      byebug
+      response
+
   end
 
 
