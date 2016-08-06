36f1704934b896d0ea90d6e12412c90850a92448
carlosgramos <carlosr86@hotmail.com>
2016-08-06 00:44:22 -0400

    modified locations_controller and index.html

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -3,10 +3,6 @@ class LocationsController < ApplicationController
 
   def index
 
-    @location = Location.all
-    @location = request.location
-    @location = Geocoder.search('wynwood')
-
     # https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670,151.1957&radius=500&types=food&name=cruise&key=AIzaSyAuw4no8ETufFiyu3FrBgOf4_rtZODlIoU
   end
 
@@ -14,9 +10,12 @@ class LocationsController < ApplicationController
   end
 
   def show
-    @location = request.location
-    @location = Geocoder.search('wynwood')
-      @location =  HTTParty.get("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670,151.1957&radius=500&types=food&name=cruise&key=AIzaSyAuw4no8ETufFiyu3FrBgOf4_rtZODlIoU")
+    #@location = request.location
+    @location = Geocoder.search(params[:search_locations])
+    p @location
+    lat @location[0].latitude
+    long @location[0].longitude
+    response =  HTTParty.get("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670,151.1957&radius=500&types=food&name=cruise&key=AIzaSyAuw4no8ETufFiyu3FrBgOf4_rtZODlIoU")
   end
 
 
